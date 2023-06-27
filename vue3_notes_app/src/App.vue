14448ff6287c3ffdd371d40c6ca718407d76c684
Min Khant Naung <minkhantnaungkzw@gmail.com>
2023-06-27 19:58:41 +0630

    replace_bootstrap_modal_with_custom_modal_for_hide

--- vue3_notes_app/src/App.vue
+++ vue3_notes_app/src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 const newNote = ref('');
 const notes = ref([]);
 const errorMessage = ref('');
+const showModal = ref(false);
 
 const randomColor = () => {
   return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
@@ -21,6 +22,7 @@ const addNote = () => {
   });
   newNote.value = '';
   errorMessage.value = '';
+  showModal.value = false;
 }
 
 </script>
@@ -30,34 +32,27 @@ const addNote = () => {
     <div class="container mt-5">
       <div class="row">
         <div class="col-12 col-md-8 offset-md-2">
-          <!-- Create Modal -->
-          <div class="modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-            aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <form @submit.prevent="addNote">
-                    <textarea v-model.trim="newNote" :class="[errorMessage ? 'is-invalid' : '', 'form-control']"></textarea>
-                    <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
-                    <button type="submit" class="btn btn-primary col-12 mt-2">Add Note</button>
-                    <button type="button" class="btn btn-danger col-12 mt-2" data-bs-dismiss="modal">Close</button>
-                  </form>
-                </div>
-              </div>
+          <!-- Custom Modal -->
+          <div v-if="showModal" class="formModal" id="myModal">
+            <div class="modal-content">
+              <span @click="showModal = false" class="close">&times;</span>
+              <form @submit.prevent="addNote">
+                <textarea v-model.trim="newNote" :class="[errorMessage ? 'is-invalid' : '', 'form-control']"></textarea>
+                <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
+                <button type="submit" class="btn btn-primary col-12 mt-2">Add Note</button>
+                <button type="button" @click="showModal = false" class="btn btn-danger col-12 mt-2">Close</button>
+              </form>
             </div>
           </div>
-          <!-- Create Modal end -->
+          <!-- Custom Modal -->
 
           <h1>Notes
-            <button type="button" class="btn btn-dark rounded-circle float-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop">+</button>
+            <button type="button" @click="showModal = true" class="btn btn-dark rounded-circle float-end">+</button>
           </h1>
           <div class="card-container mt-3">
             <div class="row">
               <div v-for="note in notes" :key="note.id" class="col-md-4 col-sm-12 mt-2">
-                <div class="card" :style="{ backgroundColor: note.backgroundColor, height:'225px' }">
+                <div class="card" :style="{ backgroundColor: note.backgroundColor, height: '225px', overflow: 'auto' }">
                   <div class="card-body p-1 text-black">
                     {{ note.text }}
                   </div>
@@ -73,3 +68,41 @@ const addNote = () => {
     </div>
   </main>
 </template>
+
+<style scoped>
+.formModal {
+  /* display: none; */
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 600px;
+}
+
+.close {
+  color: #aaa;
+  text-align: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
