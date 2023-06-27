9ab3d8c01d02e94d4f5c82582832e54902c40488
Min Khant Naung <minkhantnaungkzw@gmail.com>
2023-06-27 15:35:45 +0630

    add_notes_finished_with_bootstrap_modal

--- vue3_notes_app/src/App.vue
+++ vue3_notes_app/src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
 import { ref } from 'vue';
 
-let showModal = ref(false);
+const newNote = ref('');
+const notes = ref([]);
+const errorMessage = ref('');
+
+const randomColor = () => {
+  return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+
+const addNote = () => {
+  if (newNote.value.length < 10) {
+    return errorMessage.value = 'You need to fill 10 characters or more.'
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: randomColor()
+  });
+  newNote.value = '';
+  errorMessage.value = '';
+}
 
 </script>
 
@@ -10,9 +30,8 @@ let showModal = ref(false);
     <div class="container mt-5">
       <div class="row">
         <div class="col-12 col-md-8 offset-md-2">
-
           <!-- Create Modal -->
-          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+          <div class="modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
             aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -20,41 +39,30 @@ let showModal = ref(false);
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                  <textarea class="form-control border border-3"></textarea>
-                  <button type="button" class="btn btn-primary col-12 mt-2">Add Note</button>
-                  <button type="button" class="btn btn-danger col-12 mt-2" data-bs-dismiss="modal">Close</button>
+                  <form @submit.prevent="addNote">
+                    <textarea v-model.trim="newNote" :class="[errorMessage ? 'is-invalid' : '', 'form-control']"></textarea>
+                    <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
+                    <button type="submit" class="btn btn-primary col-12 mt-2">Add Note</button>
+                    <button type="button" class="btn btn-danger col-12 mt-2" data-bs-dismiss="modal">Close</button>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
           <!-- Create Modal end -->
 
-          <h1>Notes {{ showModal }}
+          <h1>Notes
             <button type="button" class="btn btn-dark rounded-circle float-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop">+</button>
           </h1>
           <div class="card-container mt-3">
             <div class="row">
-              <div class="col-md-4 col-sm-12 mt-2">
-                <div class="card bg-warning">
-                  <div class="card-body p-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione quod dolorum error molestias
-                    nesciunt ipsa quisquam. Exercitationem doloremque quia soluta vel ipsa ab pariatur omnis quaerat,
-                    aliquid, laudantium veritatis!
+              <div v-for="note in notes" :key="note.id" class="col-md-4 col-sm-12 mt-2">
+                <div class="card" :style="{ backgroundColor: note.backgroundColor, height:'225px' }">
+                  <div class="card-body p-1 text-black">
+                    {{ note.text }}
                   </div>
-                  <div class="card-footer p-1 mt-1">
-                    <p>30/6/2023</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 col-sm-12 mt-2">
-                <div class="card bg-warning">
-                  <div class="card-body p-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ratione quod dolorum error molestias
-                    nesciunt ipsa quisquam. Exercitationem doloremque quia soluta vel ipsa ab pariatur omnis quaerat,
-                    aliquid, laudantium veritatis!
-                  </div>
-                  <div class="card-footer p-1 mt-1">
-                    <p>30/6/2023</p>
+                  <div class="card-footer p-1 mt-1 text-black">
+                    <p>{{ note.date.toLocaleDateString() }}</p>
                   </div>
                 </div>
               </div>
