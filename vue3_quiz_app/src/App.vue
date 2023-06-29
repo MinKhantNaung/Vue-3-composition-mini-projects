062b9aab8b664c5f225ec28dd24db963d5c16fbe
Min Khant Naung <minkhantnaungkzw@gmail.com>
2023-06-29 10:24:56 +0630

    separate_component_card_&_pass_quiz_with_prop

--- vue3_quiz_app/src/App.vue
+++ vue3_quiz_app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, watch } from 'vue';
 import q from './data/quizes.json'
+import Card from './components/Card.vue'
 
 const quizes = ref(q);
 const search = ref('');
@@ -17,13 +18,7 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img">
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -55,29 +50,5 @@ watch(search, () => {
     display: flex;
     flex-wrap: wrap;
     margin-top: 40px;
-  }
-
-  /* Card */
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-    margin: 0 20px 35px 0;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-  }
-
-  .card .card-text {
-    padding: 0 5px;
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
   }
 </style>
---
+++ vue3_quiz_app/src/components/Card.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+const {quiz} = defineProps(['quiz']);
+
+console.log(quiz)
+</script>
+
+<template>
+    <div>
+        <div class="card">
+            <img :src="quiz.img">
+            <div class="card-text">
+                <h2>{{ quiz.name }}</h2>
+                <p>{{ quiz.questions.length }} questions</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* Card */
+.card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin: 0 20px 35px 0;
+    cursor: pointer;
+}
+
+.card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+}
+
+.card .card-text {
+    padding: 0 5px;
+}
+
+.card .card-text h2 {
+    font-weight: bold;
+}
+</style>
