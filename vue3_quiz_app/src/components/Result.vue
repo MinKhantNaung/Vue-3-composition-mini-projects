e15982b6983f4a4bf76bc63b046c54bb4153a9ed
Min Khant Naung <minkhantnaungkzw@gmail.com>
2023-06-30 10:44:09 +0630

    show_result_(All_finished)

---
+++ vue3_quiz_app/src/components/Result.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+const { numberOfCorrectAnswers, quizQuestionsLength } = defineProps(['numberOfCorrectAnswers', 'quizQuestionsLength']);
+
+</script>
+
+<template>
+    <div class="results">
+        <p>Your Results...</p>
+        <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionsLength }}</h1>
+        <RouterLink to="/">Go back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.results {
+    text-align: center;
+    padding: 100px 0;
+}
+
+p {
+    font-size: 25px;
+}
+
+h1 {
+    font-size: 80px;
+}
+</style>
--- vue3_quiz_app/src/views/Quiz.vue
+++ vue3_quiz_app/src/views/Quiz.vue
@@ -2,8 +2,9 @@
 import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import quizes from '../data/quizes.json'
-import QuizHeader from '../components/QuizHeader.vue'
+import QuizHeader from '../components/QuizHeader.vue';
 import Question from '../components/Question.vue';
+import Result from '../components/Result.vue';
 
 const route = useRoute();
 
@@ -27,13 +28,20 @@ const onOptionSelected = (isCorrect) => {
         numberOfCorrectAnswers.value++;
     }
     currentQuestionIndex.value++;
+
+    if (currentQuestionIndex.value === quiz.questions.length) {
+        showResult.value = true;
+    }
 }
+
+// for show result
+const showResult = ref(false);
 </script>
 
 <template>
     <div>
         <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
-        <Question @selectOption="onOptionSelected" :question="quiz.questions[currentQuestionIndex]" />
-        <button @click="currentQuestionIndex++">Next Question >></button>
+        <Question v-if="!showResult" @selectOption="onOptionSelected" :question="quiz.questions[currentQuestionIndex]" />
+        <Result v-else :numberOfCorrectAnswers="numberOfCorrectAnswers" :quizQuestionsLength="quiz.questions.length" />
     </div>
 </template>
